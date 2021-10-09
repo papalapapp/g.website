97af03dea62f7f2ef8c5f460db5ffa96b2ce5e60
Peter De bondt <peter@MacBook-Air-von-Peter.local>
2021-10-09 22:06:01 +0200

    Version 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger-button');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burger.addEventListener('click',()=>{
+		nav.classList.toggle('nav-active');
+
+		burger.classList.toggle('toggle');
+	});
+
+}
+
+navSlide();
+
+console.log("hello")
